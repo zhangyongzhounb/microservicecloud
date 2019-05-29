6806969f41f452efbdca18cb19dba5ed1eeb94d2
yongzhou.zhang <yongzhou.zhang@mtime.com>
2019-05-29 18:51:38 +0800

    parent--API--Provider--client-eureka

--- microservicecloud-provider-dept-8001/src/main/java/com/zyz/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/zyz/springcloud/DeptProvider8001_App.java
@@ -2,11 +2,13 @@ package com.zyz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by yongzhou.zhang on 2019/5/28.
  */
 @SpringBootApplication
+@EnableEurekaClient  //本服务启动后会自动注册进eureka服务中
 public class DeptProvider8001_App {
 
     public static void main(String[] args) {
